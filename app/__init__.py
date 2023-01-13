ae4100babb4d6c9c3f93dc10a40ab9d93d6c58e4
Jeremiah <jeremiahlu09@gmail.com>
2023-01-12 20:05:11 -0500

    package.json

--- app/__init__.py
+++ app/__init__.py
@@ -13,8 +13,8 @@ from .api.cart_routes import cart_routes
 from .seeds import seed_commands
 from .config import Config
 
+# app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
 app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
-
 # Setup login manager
 login = LoginManager(app)
 login.login_view = 'auth.unauthorized'
