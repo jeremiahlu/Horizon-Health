926630514d494c5bf109489ca44833de73c0922a
Jeremiah <jeremiahlu09@gmail.com>
2023-01-17 07:37:15 -0500

    render

--- app/__init__.py
+++ app/__init__.py
@@ -58,7 +58,7 @@ CORS(app)
 @app.after_request
 def inject_csrf_token(response):
     response.set_cookie(
-        'csrf_token',
+        'csrf_token', 
         generate_csrf(),
         secure=True if os.environ.get('FLASK_ENV') == 'production' else False,
         samesite='Strict' if os.environ.get(
